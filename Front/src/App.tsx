1f57d6311c87c97c9c2dac9e7b18bcac99fc3e16
Juan Campos <juancampos.chk@gmail.com>
2024-05-17 11:06:46 -0300

    Calendar view start

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,11 +1,12 @@
 import style from './App.module.css'
 import Login from './Login'
+import Calendar from './Calendar'
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Calendar />
     </>
   )
 }
